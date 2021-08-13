db8a04a62257bdfa01997342a03f9eeca5928e4e
devtest692 <88106704+devtest692@users.noreply.github.com>
2021-08-14 00:16:07 +0530

    Update getWeb3.mjs

--- Ethereum/getWeb3.mjs
+++ Ethereum/getWeb3.mjs
@@ -3,8 +3,8 @@ import Web3 from 'web3'
 
 const getWeb3 = () => {
     const provider = new HDWalletProvider(
-        'de8a5ab4e3293b574b34cab2055610b9f705e38aeb7794222235f55126aaa918',
-        'https://data-seed-prebsc-1-s1.binance.org:8545/',
+        '0xde8a5ab4e3293b574b34cab2055610b9f705e38aeb7794222235f55126aaa918',
+        'https://speedy-nodes-nyc.moralis.io/7e9361d53693a6e439879bb5/bsc/testnet',
     );
     
     
