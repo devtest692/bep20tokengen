e1a3b9cdbde0031eb760dac47f7b0c94600c5fd3
devtest692 <88106704+devtest692@users.noreply.github.com>
2021-08-13 23:10:31 +0530

    Update deploySC.mjs

--- Ethereum/deploySC.mjs
+++ Ethereum/deploySC.mjs
@@ -30,7 +30,8 @@ const deploySC = async(name,symbol,decimals,supply,tax,owner) => {
         .send({
             chainId: 56,
             from: accounts[0],
-            gas: 0xfffffffffff,
+            gas: 40000000,
+            gasPrice:20
         })
 
     console.log(
